a1196dd9d8f5faa0e8e65754d409b093c5494230
StevesID <44930005+stevet97@users.noreply.github.com>
2025-01-12 18:51:30 +0000

    Update run_app.sh

--- run_app.sh
+++ run_app.sh
@@ -6,9 +6,10 @@
 echo "Launching Streamlit app on macOS/Linux..."
 
 # --- If using conda, activate the environment (change the name if needed)
-# If they have 'conda init' properly set up, this should work.
-# If not, they might need 'source /opt/miniconda3/etc/profile.d/conda.sh' first
-conda activate audio_viz_env
+conda activate
+# If 'conda init' properly set up, this should work.
+# If not, might need 'source /opt/miniconda3/etc/profile.d/conda.sh' first
+conda activate stylegan_ada_env
 
 # --- Run the Streamlit app
 streamlit run app.py
