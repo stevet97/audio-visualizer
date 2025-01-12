70bb55413ee95b6c2efac6202f81a3feb4ba4991
StevesID <44930005+stevet97@users.noreply.github.com>
2025-01-12 18:46:53 +0000

    Update run_app.bat

--- run_app.bat
+++ run_app.bat
@@ -7,7 +7,7 @@ echo "Launching Streamlit app on Windows..."
 
 REM --- If using conda, activate the environment
 REM --- (Change 'audio_viz_env' to your actual env name)
-call conda activate audio_viz_env
+call conda activate stylegan_ada_env
 
 REM --- Run the Streamlit app
 streamlit run app.py
